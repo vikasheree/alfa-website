bbcb4cd22726ecec039b25fdebd28c1cabcd0e89
Vikas Mishra <vikasmishrag1470@gmail.com>
2026-08-22 19:02:52 +0530

    feat: add responsive mobile navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import './Navbar.css'
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -9,7 +12,15 @@ function Navbar() {
           ALFA
         </a>
 
-        <ul className="navbar-links">
+        <button
+          className="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+        >
+          {menuOpen ? '✕' : '☰'}
+        </button>
+
+        <ul className={`navbar-links ${menuOpen ? 'mobile-open' : ''}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#industrial">Industrial</a></li>
           <li><a href="#duro-mats">Duro Mats</a></li>
